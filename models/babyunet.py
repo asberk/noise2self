137498bf420b2d9a0b1939e15d530dc9c8a8a4bf
Aaron Berk (grimpoteuthis) <aberk@math.ubc.ca>
2020-05-25 16:24:45 -0700

    recent commit before laptop repair

--- models/babyunet.py
+++ models/babyunet.py
@@ -11,16 +11,20 @@ class BabyUnet(nn.Module):
         self.pool1 = nn.MaxPool2d(kernel_size=2)
         self.pool2 = nn.MaxPool2d(kernel_size=2)
 
-        self.up1 = lambda x: F.interpolate(x, mode='bilinear', scale_factor=2, align_corners=False)
-        self.up2 = lambda x: F.interpolate(x, mode='bilinear', scale_factor=2, align_corners=False)
+        self.up1 = lambda x: F.interpolate(
+            x, mode="bilinear", scale_factor=2, align_corners=False
+        )
+        self.up2 = lambda x: F.interpolate(
+            x, mode="bilinear", scale_factor=2, align_corners=False
+        )
 
         self.conv1 = ConvBlock(n_channel_in, width)
-        self.conv2 = ConvBlock(width, 2*width)
+        self.conv2 = ConvBlock(width, 2 * width)
 
-        self.conv3 = ConvBlock(2*width, 2*width)
+        self.conv3 = ConvBlock(2 * width, 2 * width)
 
-        self.conv4 = ConvBlock(4*width, 2*width)
-        self.conv5 = ConvBlock(3*width, width)
+        self.conv4 = ConvBlock(4 * width, 2 * width)
+        self.conv5 = ConvBlock(3 * width, width)
 
         self.conv6 = nn.Conv2d(width, n_channel_out, 1)
 
@@ -29,7 +33,7 @@ class BabyUnet(nn.Module):
         x = self.pool1(c1)
         c2 = self.conv2(x)
         x = self.pool2(c2)
-        self.conv3(x)
+        x = self.conv3(x)
 
         x = self.up1(x)
         x = torch.cat([x, c2], 1)
